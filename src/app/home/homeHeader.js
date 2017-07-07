eb19dd37a4ce7ab94fff9b03bd62d4161a240a47
mdresvyankin <mdresvyankin@gmail.com>
2017-07-07 14:35:24 +0300

    Added header component with navigation menu

---
+++ src/app/home/homeHeader.js
@@ -0,0 +1,39 @@
+/**
+ * Created by mdresvyankin on 07.07.17.
+ */
+angular
+  .module('home')
+  .component('homeHeader', {
+    templateUrl: ['$element', '$attrs', 'isAuthService', ($element, $attrs, isAuthService) => {
+      return `app/home/homeHeader.html`;
+    }],
+    controller: homeHeaderController
+  });
+
+function homeHeaderController(isAuthService,$filter) {
+  const vm = this;
+  const menu = [
+    {
+      id: 2,
+      state: 'home.login',
+      name: 'Вхід',
+      registered: false,
+      unRegistered: true
+    },
+    {
+      id: 3,
+      state: 'home.register',
+      name: 'Реєстрація',
+      registered: false,
+      unRegistered: true
+    },
+    {
+      id: 1,
+      state: 'home',
+      name: 'Головна',
+      registered: true,
+      unRegistered: true
+    }];
+  vm.isAuth = isAuthService.authenticated() ? {registered:true} : {unRegistered:true};
+  vm.showMenu = $filter('filter')(menu, vm.isAuth);
+}
--- src/routes.js
+++ src/routes.js
@@ -9,13 +9,7 @@ angular
 /** @ngInject */
 function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(false).hashPrefix('!');
-  $urlRouterProvider.otherwise('/');
-
-  $stateProvider
-    .state('app', {
-      url: '/',
-      component: 'login'
-    });
+  $urlRouterProvider.otherwise('/home');
 }
 
 function homeRoutesConfig($stateProvider) {
@@ -28,5 +22,13 @@ function homeRoutesConfig($stateProvider) {
       url: '/details/:filmId',
       component: 'filmDetail',
       cache: false
+    })
+    .state('home.login', {
+      url: '/login',
+      component: 'login'
+    })
+    .state('home.register', {
+      url: '/register',
+      component: 'register'
     });
 }
